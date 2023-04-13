b1dda547407d0299d61523c068959d7deae49d67
sediqbadakhsh <sediq@invasso.com>
2023-04-13 20:53:09 +0430

    configure routes for the new actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   mount Rswag::Api::Engine => '/api-docs'
   namespace :api do
     namespace :v1 do
+      post "search", to: "search_queries#create"
       get "search", to: "search_queries#search"
+      get "search/top_keywords", to: "search_queries#keywords"
+      get "search/most_asked_by_users", to: "search_queries#most_asked_by_users"
+      get "search/searches_by_hour_in_week", to: "search_queries#searches_by_hour_in_week"
     end
   end
 end
