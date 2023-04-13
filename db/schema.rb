846e123b56ebd40c2a67112dff946f637cf2c444
sediqbadakhsh <sediq@invasso.com>
2023-04-13 20:52:10 +0430

    add faker and add fake data in seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_12_010004) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_13_100423) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_12_010004) do
     t.string "ip_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "counter", default: 0
     t.index ["query"], name: "index_search_queries_on_query"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Faker::Config.locale = :en
+
+15.times do
+    SearchQuery.create!(query: "What tech stacks are Helpjuice working with?", ip_address: Faker::Internet.ip_v4_address, counter: 1)
+end
+
+10.times do
+  SearchQuery.create!(query: "Who is Emil Hajric?", ip_address: Faker::Internet.ip_v4_address, counter: 1)
+end
+
+15.times do
+  SearchQuery.create!(query: "What is Helpjuice?", ip_address: Faker::Internet.ip_v4_address, counter: 1)
+end
+
+5.times do
+  SearchQuery.create!(query: "Who is the owner of Helpjuice", ip_address: Faker::Internet.ip_v4_address, counter: 1)
+end
+
+7.times do
+  SearchQuery.create!(query: "What are the services of Helpjuice company?", ip_address: Faker::Internet.ip_v4_address, counter: 1)
+end
+
+25.times do
+  SearchQuery.create!(query: "What it takes to be part of Helpjuice Amazing team?", ip_address: Faker::Internet.ip_v4_address, counter: 1)
+end
