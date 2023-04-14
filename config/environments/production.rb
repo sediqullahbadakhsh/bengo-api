6164c02201490c276cdfd79f8ac7a73c11a556db
sediq <sediq@invasso.com>
2023-04-14 19:29:36 +0430

    update configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,5 +86,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   # config.require_master_key = true
-  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 end
